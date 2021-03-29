2fed3c83a432c1a63a27c05e5ae59f05915d67a0
Ivan Petkov <ipetkow90@gmail.com>
2021-03-29 22:25:54 +0300

    adding classwork-gradle-demo

---
+++ 04. Automation with Java/classwork-gradle-demo/src/test/java/SmokeTests.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SmokeTests {
+
+    @Test
+    @DisplayName("My Smoke Test 1")
+    public void testFeature1() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("My Smoke Test 2")
+    public void testFeature2() {
+        assertTrue(true);
+    }
+
+}
